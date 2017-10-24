48de348e52a130170a95536258b5cce19994ea03
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2017-10-25 00:36:28 +0300

    Added flow types to util functions

--- src/util.js
+++ src/util.js
@@ -6,10 +6,14 @@ const { emit } = EventEmitter.prototype;
 
 // A simple UID creation function
 let lastUID = -1;
-export const uid = () => {
+export const uid = (prefix: string = 'w'): string => {
   lastUID += 1;
-  return `w${Math.floor(46656 + (Math.random() * 1632959)).toString(36)}${lastUID.toString(36)}`;
+  return `${prefix}${Math.floor(46656 + (Math.random() * 1632959)).toString(36)}${lastUID.toString(36)}`;
 };
 
 // Emit an event on an object
-export const emitOn = (object: any, name: string, data: any) => emit.call(object, name, data);
+export const emitOn = (
+  object: any,
+  name: string,
+  data: any
+): boolean => emit.call(object, name, data);
