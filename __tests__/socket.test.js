1506d518c01b9085a6765d5ec7cdf26c8c14d152
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2017-11-03 11:15:21 +0200

    Added tests for socket setHandlers function

--- __tests__/socket.test.js
+++ __tests__/socket.test.js
@@ -1,10 +1,14 @@
-import { forEachMessage, setEncoding, writeMessage } from '../src/socket';
+import { forEachMessage, setEncoding, attachHandlers, writeMessage } from '../src/socket';
 import MockSocket from '../__mocks__/socket';
 
-describe('forEachMessage function', () => {
-  const mockMessage = '{"name": "msg1", "data": "data1"}\n{"name": "msg2", "data": "data2"}';
-  const mockBadMessage = '{"name" "msg1", "data": "data1"}\n{"name": "msg2", "data": "data2"}';
+const mockMessage = '{"name": "msg1", "data": "data1"}\n{"name": "msg2", "data": "data2"}';
+const mockBadMessage = '{"name" "msg1", "data": "data1"}\n{"name": "msg2", "data": "data2"}';
+const mockData = {
+  name: 'msg1',
+  data: 'hello\nworld'
+};
 
+describe('forEachMessage function', () => {
   test('should call callback for each message line', () => {
     const func = jest.fn();
     forEachMessage(mockMessage, func);
@@ -29,14 +33,26 @@ describe('setEncoding function', () => {
   });
 });
 
-// TODO: attachHandlers tests
+describe('attachHandlers function', () => {
+  const mockSocket = new MockSocket();
+
+  test('should not throw', () => {
+    const attach = () => attachHandlers(mockSocket);
+    expect(attach).not.toThrow();
+  });
+
+  test('set data handler should not throw', () => {
+    const emitData = () => mockSocket.emit('data', mockMessage);
+    expect(emitData).not.toThrow();
+  });
+
+  test('set close handler should not throw', () => {
+    const emitClose = () => mockSocket.emit('close', null);
+    expect(emitClose).not.toThrow();
+  });
+});
 
 describe('writeMessage function', () => {
-  const mockData = {
-    name: 'msg1',
-    data: 'hello\nworld'
-  };
-
   test('properly write data to socket', () => {
     const result = writeMessage(MockSocket, mockData);
     expect(result.message).toBe(`${JSON.stringify(mockData)}\n`);
