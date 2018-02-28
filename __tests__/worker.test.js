c0c633126826fe1e6b6acbdbfaad8889c2189f38
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2018-02-28 16:52:51 +0200

    Added some worker tests

--- __tests__/worker.test.js
+++ __tests__/worker.test.js
@@ -1,4 +1,5 @@
 import init from '../src/worker';
+import MockSocket from '../__mocks__/socket';
 
 const mockExecutable = './__mocks__/rScript.sh';
 const Worker = init(mockExecutable);
@@ -7,5 +8,41 @@ describe('Worker', () => {
   test('should throw when initialised with bad path', () => {
     const initialise = () => new Worker('xxx');
     expect(initialise).toThrow();
+  });
+
+  test('should initialize', () => {
+    const worker = new Worker(mockExecutable);
+    expect(worker).toBeInstanceOf(Worker);
+  });
+});
+
+describe('emit', () => {
+  const worker = new Worker(mockExecutable);
+  const socket = new MockSocket();
+
+  test('should return false if worker is not alive', () => {
+    worker.alive = false;
+    const result = worker.emit('name', 1, 2, 3);
+    expect(result).toBe(false);
+    worker.alive = true;
+  });
+
+  test('should push messages to message queue if no socket is present', () => {
+    expect(worker.socketQueue.length).toBe(0);
+    worker.emit('name', 1, 2, 3);
+    expect(worker.socketQueue.length).toBe(1);
+    expect(worker.socketQueue[worker.socketQueue.length - 1]).toEqual({
+      event: 'name',
+      data: [1, 2, 3]
+    });
+    worker.socketQueue = [];
+  });
+
+  test('should write to socket if one is present', () => {
+    socket.write = jest.fn();
+    worker.attachSocket(socket);
+    worker.emit('name', 1, 2, 3);
+    expect(socket.write).toHaveBeenCalledTimes(1);
+    expect(socket.write).toHaveBeenCalledWith('{"event":"name","data":[1,2,3]}\n', 'utf8');
   });
 });
